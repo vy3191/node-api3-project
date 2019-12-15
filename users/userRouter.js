ccf3b2066d8a21e5da4ad74c4f38ca7e1d59ded5
vy3191 <venkyafhs@gmail.com>
2019-12-15 17:50:59 -0500

    completed middleware part

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require("./userDb");
 
 const router = express.Router();
 
@@ -33,15 +34,33 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const {id} = req.params;
+  db.getById(id)
+    .then( (user) => {
+       if(user) {
+          req.user = user;
+          next()
+       } else {
+         res.status(404).json({ message: "invalid user id" });
+       }
+    }).catch(err => {
+       res.status(500).json({msg:`Something went wrong`})
+    })
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  const { id , name } = req.body;
+  if(!id && !name ) res.status(400).json({ message: "missing user data" });
+  if(!name) res.status(400).json({ message: "missing required name field" });
+  next();
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  const { body } = req;
+  if(body === "undefined") res.json(400).json({ message: "missing post data" });
+  if(!body.text) res.json(400).json({message: "missing required text field" })
 }
 
 module.exports = router;
