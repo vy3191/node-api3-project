18031f69a189ff07e826d3e52e1b2d04abb2f9c7
vy3191 <venkyafhs@gmail.com>
2019-12-15 17:11:21 -0500

    Initial commit & project dependencies are added

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code away!
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const PORT = 8000;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.status(200).json({
+       Message: `app is up and running now.`
+    })
+})
+
+app.listen(PORT, () => {
+   console.log(`Server is running on http://localhost:${PORT}`);
+})
